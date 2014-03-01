dd36dd92abc8f44b9956c112f5c3eddca7374c58
Dhruv <amit@ubuntu.(none)>
2014-03-01 21:33:45 +0530

    file added

---
+++ makelink.py
@@ -0,0 +1,17 @@
+import os
+import mechanize
+
+br=mechanize.Browser()
+br.set_handle_robots(False)
+br.open('http://pastebin.ubuntu.com')
+br.select_form(nr=0)
+path=raw_input("enter file path - ")
+s=open(path,"r").read()
+br.form["poster"]=os.popen("whoami").read()
+#br.form["syntax"]="text"
+br.form["content"]=s
+br.submit()
+
+l=[x for x in br.links()]
+print "http://pastebin.ubuntu.com"+l[0].url[:len(l[0].url)-6]
+
